34bac20e874515db2c1c9ddf756470ef2ad2fc12
Roger <rogerjaffe@gmail.com>
2019-04-27 17:46:22 -0700

    Reopen list when going back from viewing student work

--- ExtensionFiles/openStudents.js
+++ ExtensionFiles/openStudents.js
@@ -1,16 +1,21 @@
+let viewOpen = false;
 $(document).ready(function() {
-	setTimeout(poll, 300);
+	setTimeout(poll, 1000);
 });
-var pollCount = 0, maxPoll = 10;
 var poll = function() {
 	let b = document.getElementsByClassName('cb-expand-button');
 	if (b.length > 0) {
-		b[0].click();
-		pollCount == 10;
-	} else {
-		if (pollCount < maxPoll) {
-			setTimeout(poll, 300);
+		let url = document.location.href;
+		if (url.indexOf('#instruction') >= 0) {
+			if (!viewOpen) {
+				b[0].click();
+				viewOpen = true;				
+			}
+		} else {
+			viewOpen = false;
 		}
+	} else {
+		viewOpen = false;
 	}
-	pollCount++;
+	setTimeout(poll, 1000);
 }
